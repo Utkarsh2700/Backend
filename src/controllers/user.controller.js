0fad42fbaccbfbda98f0434b21f72613dca40a67
Utkarsh 2700 <utkarshsharma2718@gmail.com>
2024-08-22 20:50:26 +0530

    fixed issue in logout method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -183,8 +183,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
